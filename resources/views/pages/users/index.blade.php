f887dc242935e5f9bb20c5f0e545592379bbcac4
Mohammad Karam Ahmad <karamma3rouf@gmail.com>
2025-10-23 22:45:18 +0300

    feat(users): add initial user management views and routes
    - Create empty partial views for user form, show and deleted states
    - Add route for users index page
    - Implement basic users index view with layout inheritance

---
+++ resources/views/pages/users/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('title')
+    {{__('Users.list')}}
+@endsection
+@section('subTitle')
+    {{ __('Users.list') }}
+@endsection
+@section('breadcrumb')
+    {{ __('Users') }}
+@endsection
+@section('breadcrumbActive')
+    {{ __('Users') }}
+@endsection
+@section('content')
+test
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layouts.app');
 });
+Route::get('/users', function () {
+    return view('pages.users.index');
+});

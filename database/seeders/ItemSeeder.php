064834c09c849e2d28a4c71cbc322bd4d45a01d0
Mohammad Karam Ahmad <karamma3rouf@gmail.com>
2025-10-23 12:47:35 +0300

    refactor(database): restructure seeders to use dedicated seeder classes
    Move user and item seeding logic from DatabaseSeeder to dedicated seeder classes
    Add test users and items with predefined values for consistent testing

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Item;
 
 class ItemSeeder extends Seeder
 {
@@ -12,6 +13,23 @@ class ItemSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Item::create([
+            'name' => 'Item 1',
+            'description' => 'Description 1',
+            'price' => 100.00,
+            'code' => 'ITEM001',
+            'quantity' => 10,
+            'is_active' => true,
+            'discount' => 0.00,
+        ]);
+        Item::create([
+            'name' => 'Item 2',
+            'description' => 'Description 2',
+            'price' => 200.00,
+            'code' => 'ITEM002',
+            'quantity' => 20,
+            'is_active' => true,
+            'discount' => 10.00,
+        ]);
     }
 }

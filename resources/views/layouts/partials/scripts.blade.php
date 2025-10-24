a74e37d47c3f9d04c59a45c465b5689d474eb09b
Mohammad Karam Ahmad <karamma3rouf@gmail.com>
2025-10-24 17:16:16 +0300

    refactor(assets): use Laravel asset helper for resource paths
    Update asset references in views to use Laravel's asset helper for better path resolution and cache busting. Also add Cairo font styling for consistent typography.

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -1,43 +1,43 @@
     <!-- jquery-->
-    <script src="../assets/js/vendors/jquery/jquery.min.js"></script>
+    <script src="{{ asset('assets/js/vendors/jquery/jquery.min.js') }}"></script>
     <!-- bootstrap js-->
-    <script src="../assets/js/vendors/bootstrap/dist/js/bootstrap.bundle.min.js" defer=""></script>
-    <script src="../assets/js/vendors/bootstrap/dist/js/popper.min.js" defer=""></script>
+    <script src="{{ asset('assets/js/vendors/bootstrap/dist/js/bootstrap.bundle.min.js') }}" defer=""></script>
+    <script src="{{ asset('assets/js/vendors/bootstrap/dist/js/popper.min.js') }}" defer=""></script>
     <!--fontawesome-->
-    <script src="../assets/js/vendors/font-awesome/fontawesome-min.js"></script>
+    <script src="{{ asset('assets/js/vendors/font-awesome/fontawesome-min.js') }}"></script>
     <!-- feather-->
-    <script src="../assets/js/vendors/feather-icon/feather.min.js"></script>
-    <script src="../assets/js/vendors/feather-icon/custom-script.js"></script>
+    <script src="{{ asset('assets/js/vendors/feather-icon/feather.min.js') }}"></script>
+    <script src="{{ asset('assets/js/vendors/feather-icon/custom-script.js') }}"></script>
     <!-- sidebar -->
-    <script src="../assets/js/sidebar.js"></script>
+    <script src="{{ asset('assets/js/sidebar.js') }}"></script>
     <!-- height_equal-->
-    <script src="../assets/js/height-equal.js"></script>
+    <script src="{{ asset('assets/js/height-equal.js') }}"></script>
     <!-- config-->
-    <script src="../assets/js/config.js"></script>
+    <script src="{{ asset('assets/js/config.js') }}"></script>
     <!-- apex-->
-    <script src="../assets/js/chart/apex-chart/apex-chart.js"></script>
-    <script src="../assets/js/chart/apex-chart/stock-prices.js"></script>
+    <script src="{{ asset('assets/js/chart/apex-chart/apex-chart.js') }}"></script>
+    <script src="{{ asset('assets/js/chart/apex-chart/stock-prices.js') }}"></script>
     <!-- scrollbar-->
-    <script src="../assets/js/scrollbar/simplebar.js"></script>
-    <script src="../assets/js/scrollbar/custom.js"></script>
+    <script src="{{ asset('assets/js/scrollbar/simplebar.js') }}"></script>
+    <script src="{{ asset('assets/js/scrollbar/custom.js') }}"></script>
     <!-- slick-->
-    <script src="../assets/js/slick/slick.min.js"></script>
-    <script src="../assets/js/slick/slick.js"></script>
+    <script src="{{ asset('assets/js/slick/slick.min.js') }}"></script>
+    <script src="{{ asset('assets/js/slick/slick.js') }}"></script>
     <!-- data_table-->
-    <script src="../assets/js/js-datatables/datatables/jquery.dataTables.min.js"></script>
+    <script src="{{ asset('assets/js/js-datatables/datatables/jquery.dataTables.min.js') }}"></script>
     <!-- page_datatable-->
-    <script src="../assets/js/js-datatables/datatables/datatable.custom.js"></script>
+    <script src="{{ asset('assets/js/js-datatables/datatables/datatable.custom.js') }}"></script>
     <!-- page_datatable1-->
-    <script src="../assets/js/js-datatables/datatables/datatable.custom1.js"></script>
+    <script src="{{ asset('assets/js/js-datatables/datatables/datatable.custom1.js') }}"></script>
     <!-- page_datatable-->
-    <script src="../assets/js/datatable/datatables/datatable.custom.js"></script>
+    <script src="{{ asset('assets/js/datatable/datatables/datatable.custom.js') }}"></script>
     <!-- theme_customizer-->
-    <script src="../assets/js/theme-customizer/customizer.js"></script>
+    <script src="{{ asset('assets/js/theme-customizer/customizer.js') }}"></script>
     <!-- tilt-->
-    <script src="../assets/js/animation/tilt/tilt.jquery.js"></script>
+    <script src="{{ asset('assets/js/animation/tilt/tilt.jquery.js') }}"></script>
     <!-- page_tilt-->
-    <script src="../assets/js/animation/tilt/tilt-custom.js"></script>
+    <script src="{{ asset('assets/js/animation/tilt/tilt-custom.js') }}"></script>
     <!-- dashboard_1-->
-    <script src="../assets/js/dashboard/dashboard_1.js"></script>
+    <script src="{{ asset('assets/js/dashboard/dashboard_1.js') }}"></script>
     <!-- custom script -->
-    <script src="../assets/js/script.js"></script>
+    <script src="{{ asset('assets/js/script.js') }}"></script>
--- resources/views/layouts/partials/styles.blade.php
+++ resources/views/layouts/partials/styles.blade.php
@@ -1,24 +1,35 @@
     <!-- Favicon icon-->
-    <link rel="icon" href="../assets/images/favicon.png" type="image/x-icon"/>
-    <link rel="shortcut icon" href="../assets/images/favicon.png" type="image/x-icon"/>
-    <!-- Google font-->
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=""/>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,200;6..12,300;6..12,400;6..12,500;6..12,600;6..12,700;6..12,800;6..12,900;6..12,1000&amp;display=swap" rel="stylesheet"/>
+    <link rel="icon" href="{{ asset('assets/images/favicon.png') }}" type="image/x-icon"/>
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/x-icon"/>
+
     <!-- Flag icon css -->
-    <link rel="stylesheet" href="../assets/css/vendors/flag-icon.css"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/vendors/flag-icon.css') }}"/>
     <!-- iconly-icon-->
-    <link rel="stylesheet" href="../assets/css/iconly-icon.css"/>
-    <link rel="stylesheet" href="../assets/css/bulk-style.css"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/iconly-icon.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/bulk-style.css') }}"/>
     <!-- iconly-icon-->
-    <link rel="stylesheet" href="../assets/css/themify.css"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/themify.css') }}"/>
     <!--fontawesome-->
-    <link rel="stylesheet" href="../assets/css/fontawesome-min.css"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome-min.css') }}"/>
     <!-- Whether Icon css-->
-    <link rel="stylesheet" type="text/css" href="../assets/css/vendors/weather-icons/weather-icons.min.css"/>
-    <link rel="stylesheet" type="text/css" href="../assets/css/vendors/scrollbar.css"/>
-    <link rel="stylesheet" type="text/css" href="../assets/css/vendors/slick.css"/>
-    <link rel="stylesheet" type="text/css" href="../assets/css/vendors/slick-theme.css"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/weather-icons/weather-icons.min.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/scrollbar.css') }}"/>
+    <!-- Slick css -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/slick.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/slick-theme.css') }}"/>
     <!-- App css -->
-    <link rel="stylesheet" href="../assets/css/style.css"/>
-    <link id="color" rel="stylesheet" href="../assets/css/color-1.css" media="screen"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}"/>
+    <link id="color" rel="stylesheet" href="{{ asset('assets/css/color-1.css') }}" media="screen"/>
+
+ <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
+<style>
+    *:not(i) {
+        font-family: "Cairo", sans-serif !important;
+        font-optical-sizing: auto !important;
+        font-weight: 700 !important;
+        font-style: normal !important;
+        font-variation-settings:"slnt" 0 !important;
+    }
+</style>

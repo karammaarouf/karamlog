7ded07ab098168af65a5d001de7aa43f81b151e7
Mohammad Karam Ahmad <karamma3rouf@gmail.com>
2025-12-03 11:47:09 +0300

    style(buttons): remove sm class from create button to match design

--- resources/views/components/buttons/create.blade.php
+++ resources/views/components/buttons/create.blade.php
@@ -2,4 +2,4 @@
     'action' => '',
     'text' => 'create',
 ])
-<a href="{{ $action }}" class="btn btn-sm btn-outline-success">{{ __($text) }}</a>
+<a href="{{ $action }}" class="btn btn-outline-success">{{ __($text) }}</a>

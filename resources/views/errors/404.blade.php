cb20ed8fbecc8d3129d702c3f2898981eb10c2dc
Mohammad Karam Ahmad <karamma3rouf@gmail.com>
2025-12-26 21:59:52 +0300

    feat(i18n): add arabic translations for error pages
    Add missing Arabic translations for error pages (400, 404, 500, 503) and update views to use localization

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -234,8 +234,8 @@
             </svg>
           </div>
           <div class="col-md-8 offset-md-2">
-            <h3>Oops! This Page is Not Found.</h3>
-            <p class="sub-content">The page you are attempting to reach is currently not available. This may be because the page does not exist or has been moved.</p><a class="btn btn-primary" href="{{ route('dashboard.index') }}">BACK TO HOME PAGE</a>
+            <h3>{{__('Oops! This Page is Not Found.')}}</h3>
+            <p class="sub-content">{{__('The page you are attempting to reach is currently not available. This may be because the page does not exist or has been moved.')}}</p><a class="btn btn-primary" href="{{ route('dashboard.index') }}">{{__('BACK TO HOME PAGE')}}</a>
           </div>
         </div>
       </div>
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -501,8 +501,8 @@
             </svg>
           </div>
           <div class="col-md-8 offset-md-2">
-            <h3>Internal Server Error</h3>
-            <p class="sub-content">The page you are attempting to reach is currently not available. This may be because the page does not exist or has been moved.</p><a class="btn btn-primary" href="{{ route('dashboard.index') }}">BACK TO HOME PAGE</a>
+            <h3>{{__('Internal Server Error')}}</h3>
+            <p class="sub-content">{{__('The page you are attempting to reach is currently not available. This may be because the page does not exist or has been moved.')}}</p><a class="btn btn-primary" href="{{ route('dashboard.index') }}">{{__('BACK TO HOME PAGE')}}</a>
           </div>
         </div>
       </div>
